8632e98116159344b811ed73d3b7ab3ab7459279
William Högman <william@hogman.me>
2013-01-08 10:54:35 +0100

    added initial files

---
+++ tictactoe/src/main/java/se/whn/tictactoe/App.java
@@ -0,0 +1,13 @@
+package se.whn.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
