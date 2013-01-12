7cc4595747cde6889ad6e1c76006bfef6c7e9587
William Hogman <me@whn.se>
2013-01-12 23:42:25 +0100

    changes to main routine

--- tictactoe/src/main/java/se/whn/tictactoe/App.java
+++ tictactoe/src/main/java/se/whn/tictactoe/App.java
@@ -12,7 +12,10 @@ public class App
 
     public App() {
 	//runAiGame();
-        testAIStrength();
+        //testAIStrength(new AlgoAI());
+        testAIStrength(new FeedForwardNNAI());
+        //findLosingGame();
+        //findLostingGameVHeuristicAI();
     }
 
 
@@ -33,16 +36,19 @@ public class App
 	}
     }
 
-    public void testAIStrength() {
-        Player p1 = new Player(new AlgoAI());
+    public void testAIStrength(Actor underTest) {
+        Player p1 = new Player(underTest);
         Player p2 = new Player(new RandomAI());
+        playLoadsOfGames(p1, p2);
+    }
 
+    private void playLoadsOfGames(Player p1, Player p2) {
         int won = 0;
         int lost = 0;
         int drawn = 0;
         
         for(int i = 0; i < 100000; i++) {
-            Game g = new Game(p1, p2);
+            Game g = new Game(p2, p1);
 
             while(!g.isGameOver()) {
                 g.playTurn();
@@ -60,5 +66,56 @@ public class App
         System.out.println("Won: " + won);
         System.out.println("Lost: " + lost);
         System.out.println("Drawn: " + drawn);
+    }
+
+    public void findLosingGame() {
+        Player p1 = new Player(new AlgoAI());
+        Player p2 = new Player(new RandomAI());
+        p1.setRenderChar('X');
+        p2.setRenderChar('O');
+
+        for(;;) {
+            Game g = new Game(p2, p1);
+            CLIGameRenderer renderer = new CLIGameRenderer(g);
+            
+
+            while(!g.isGameOver()) {
+                g.playTurn();
+                renderer.render();
+            }
+
+            Player winner = g.getWinner();
+            if(winner == p2) {
+                break;
+            }
+        }
+    }
+
+    public void findLostingGameVHeuristicAI() {
+
+        Player p1 = new Player(new AlgoAI());
+        Player p2 = new Player(new AlternatingAI(new HeuristicAI(), new RandomAI()));
+        p1.setRenderChar('X');
+        p2.setRenderChar('O');
+
+        for(;;) {
+            Game g = new Game(p2, p1);
+            CLIGameRenderer renderer = new CLIGameRenderer(g);
+            
+
+            while(!g.isGameOver()) {
+                g.playTurn();
+                renderer.render();
+            }
+
+            Player winner = g.getWinner();
+            if(winner == p2) {
+                break;
+            }            
+        }
+    }
+
+
+    public void nnAiTest() {
     }
 }
