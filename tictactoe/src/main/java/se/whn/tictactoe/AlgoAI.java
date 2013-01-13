f618aa135e3115ba15f08412ba3c3d7e37ef7209
William Hogman <me@whn.se>
2013-01-13 20:41:41 +0100

    improved algoAI

--- tictactoe/src/main/java/se/whn/tictactoe/AlgoAI.java
+++ tictactoe/src/main/java/se/whn/tictactoe/AlgoAI.java
@@ -1,5 +1,7 @@
 package se.whn.tictactoe;
 
+import java.util.Set;
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -34,15 +36,49 @@ public class AlgoAI extends HeuristicAI {
     }
 
     private Square expanding(Line[] lines, Player plr) {
-        List<Square> candidates = new ArrayList<Square>();
+        Set<Square> candidates = new HashSet<Square>();
+        Set<Square> opp = new HashSet<Square>();
+        Set<Line> forkLine = new HashSet<Line>();
+        Set<Square> cantLetPlaceIn = new HashSet<Square>();
+
         for(Line l : lines) {
             if(l.isOwnable(plr) && l.hasAny(plr)) {
                 for(Square s : l.getUnoccupied()) {
-                    candidates.add(s);
+                    if(!candidates.add(s)) {
+                        // boom headshot that's a fork!
+                        return s;
+                    }
+                }
+            } else if(l.isOwnable() && l.opponentHasAny(plr)) {
+                for(Square s : l.getUnoccupied()) {
+                    if(!opp.add(s)) {
+                        forkLine.add(l);
+                        cantLetPlaceIn.add(s);
+                    }
                 }
             }
         }
-        return this.byHeuristicScore(candidates);
+
+        if(cantLetPlaceIn.size() == 1) {
+            return cantLetPlaceIn.iterator().next();
+        }
+
+
+
+        for(Line l : lines) {
+            if(l.isOwnable(plr) && l.hasAny(plr)) {
+                boolean cantlet = false;
+                for(Square sq : l.getUnoccupied()) {
+                    if(cantLetPlaceIn.contains(sq)) {
+                        cantlet = true;
+                    }
+                }
+                if(!cantlet) {
+                    return l.getFirstUnoccupied();
+                }
+            }
+        }
+        return null;
     }
     
     /**
